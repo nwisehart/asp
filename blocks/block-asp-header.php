4c3dbbc584a7b1fbd2fbc2401aca018f2a89daa0
Nick Wisehart <NWisehart@esri.com>
2020-05-30 13:30:50 -0700

    Adds h3/4 options for asp header

--- blocks/block-asp-header.php
+++ blocks/block-asp-header.php
@@ -3,6 +3,16 @@
     $container_classes .= ' asp-color-' . block_field( 'color' , false );
     $container_classes .= ' asp-align-' . block_field( 'alignment' , false );
     $container_classes .= (block_field( 'alignment' , false ) === 'center') ? ' alignwide' : '';
-
+    $header_option = block_field( 'header-size' , false );
 ?>
-<div class="<?php echo $container_classes ?>"><h2 class="asp-header"><?php block_field( 'text' ); ?></h2></div>
+<div class="<?php echo $container_classes ?>">
+<?php 
+    if($header_option === 'h3') {
+        ?><h3 class="asp-header"><?php block_field( 'text' ); ?></h3><?php
+    } else if($header_option === 'h4') {
+        ?><h4 class="asp-header"><?php block_field( 'text' ); ?></h4><?php
+    } else {
+        ?><h2 class="asp-header"><?php block_field( 'text' ); ?></h2><?php
+    }
+?>
+</div>
