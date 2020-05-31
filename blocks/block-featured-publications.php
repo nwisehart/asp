1a15fa7b19d45ecfde67a44aec824be82370c3c1
Nick Wisehart <NWisehart@esri.com>
2020-05-31 15:43:09 -0700

    Adds functionality for faculty pages

--- blocks/block-featured-publications.php
+++ blocks/block-featured-publications.php
@@ -1,4 +1,6 @@
 <?php
+ $showAll = block_field( 'show-all' , false ) === "true" ? true : false;
+ $addClass = $showAll ? "show-all-publication-data" : "publication-title-only";
  $args = array( 
      'post_type' => array( 'asp_publications' ), 
      'posts_per_page' => block_field( 'number-of-posts' , false ), 
@@ -7,21 +9,28 @@
     echo block_field( 'tag-field' , false );
  $query = new WP_Query( $args );
      if ( $query->have_posts() ) { 
-        ?><div class="wp-block-group alignwide featured-publications"><?php
+        ?><div class="wp-block-group alignwide featured-publications <?php echo $addClass; ?>"><?php
          while ( $query->have_posts() ) {
              $query->the_post();
              ?><div class="asp-publication-container"><?php
-             if ( has_post_thumbnail()) : // Check if thumbnail exists
-                 the_post_thumbnail( 'thumbnail' );
-             endif;
-             ?>
-             <div class="asp-publication-content">
-                <div class="asp-header-container asp-color-purple asp-align-left">
-                    <h3 class="asp-header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+             if ($showAll) {
+                if ( has_post_thumbnail() ) : // Check if thumbnail exists
+                    the_post_thumbnail( 'thumbnail' );
+                endif;
+                ?>
+                <div class="asp-publication-content">
+                    <div class="asp-header-container asp-color-purple asp-align-left">
+                        <h3 class="asp-header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    </div>
+                    
+                    <?php the_excerpt(); ?>
                 </div>
-                <?php the_excerpt(); ?>
-            </div>
-            </div> <!-- close .asp-cublication-container -->
+            <?php
+            } else {
+                ?> <a class="asp-publication-list-item" href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <?php
+            }
+            ?>
+            </div> <!-- close .asp-publication-container -->
          <?php
          }
          ?> </div> <?php /* Close .wp-block-group */
