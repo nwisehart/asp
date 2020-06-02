3b239c85a5903965687cf6dbeb36ddfc3f8bfbcf
Nick Wisehart <NWisehart@esri.com>
2020-06-01 22:08:34 -0700

    Wrap header option into pub block

--- blocks/block-featured-publications.php
+++ blocks/block-featured-publications.php
@@ -3,6 +3,7 @@
  $search =  block_field( 'search' , false ) === "true" ? true : false;
  $showSearch = $showAll && $search;
  $addClass = $showAll ? "show-all-publication-data" : "publication-title-only";
+ $header = block_field( 'header' , false );
  $args = array( 
      'post_type' => array( 'asp_publications' ), 
      'posts_per_page' => block_field( 'number-of-posts' , false ), 
@@ -17,10 +18,16 @@ if ($showSearch) {
     $ID = "aspSearchItemContainer";
 }
  $query = new WP_Query( $args );
-     if ( $query->have_posts() ) { 
-        ?><div class="wp-block-group alignwide featured-publications <?php echo $addClass; ?>" id="<?php echo $ID; ?>"><?php
+     if ( $query->have_posts() ) { ?>
+        <div class="wp-block-group alignwide featured-publications <?php echo $addClass; ?>" id="<?php echo $ID; ?>"><?php
+        if(!empty( $header )) { ?>
+            <div class="asp-header-container asp-color-purple asp-align-left">
+                <h3 class="asp-header"><?php echo $header; ?></h3>
+            </div>
+        <?php }
          while ( $query->have_posts() ) {
              $query->the_post();
+             $nonSearchClass = $showSearch ? "" : "show";
              $dataSearch = $showSearch ? get_post_meta($post->ID, "Search Text", true) : "";
              $dataCategories = $showSearch ? get_the_category() : "";
              $dataCategories = array_map(
@@ -28,7 +35,7 @@ if ($showSearch) {
                     return $cat->cat_name;
                 }, $dataCategories);
              $dataCategories = implode(" ", $dataCategories);
-             ?><div class="asp-publication-container" data-categories="<?php echo $dataCategories; ?>"><?php
+             ?><div class="asp-publication-container <?php echo $nonSearchClass ?>" data-categories="<?php echo $dataCategories; ?>"><?php
              if ($showAll) {
                 if ( has_post_thumbnail() ) : // Check if thumbnail exists
                     the_post_thumbnail( 'thumbnail' );
@@ -51,11 +58,13 @@ if ($showSearch) {
          }
          ?> </div> <?php /* Close .wp-block-group */
      }
+     if ($showSearch) {
      ?>
      <div class="asp-search-container hide" id="aspSearchContainer">
         <span class="ic-search"><input id="aspSearchInput" type="text" placeholder="Search publications"></span>
     </div>
      <?php
+     }
 wp_reset_postdata();
 
 ?>
